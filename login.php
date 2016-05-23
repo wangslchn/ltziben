fdacb0d3d034bf16266a3cd61a1c685391d4e92c
wangslchn <wangsl@yeah.net>
2016-05-24 01:07:18 +0800

    数据库能够正常运行注册和登录功能

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+if ($username == "")
+{
+    //exit("用户名不能为空");
+    die('用户名不鞥你为空');
+}
+
+if($password == "")
+{
+    exit("密码不能为空");
+}
+
+$dbhost ="localhost";
+$dbuser = "ltziben";
+$dbpwd = "ltzb2016";
+$dbname = "ltziben";
+
+$connetion = mysql_connect($dbhost, $dbuser, $dbpwd);
+if($connetion){
+    
+    $password = md5($password);
+    mysql_select_db($dbname, $connetion);
+    $dbsql = "SELECT * FROM  userinfo WHERE UserName='$username' AND Password = '$password'";
+    //exit($dbsql);
+    $dbresult = mysql_query($dbsql);
+    
+    if($dbresult){
+        $row = mysql_fetch_array($dbresult);
+        if($row){
+            echo $row['UserName']."登录成功";
+            //var_dump($row);
+        }else{
+            die("用户名密码不正确，或者该用户不存在！");
+        }
+        
+    }else{
+        die("登录失败");
+    }
+}
+else {
+    echo $dbname."连接数据库失败";
+}
+
+?>
--- register.php
+++ register.php
@@ -1,8 +1,44 @@
 <?php
+
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 $password2 = $_POST['password2'];
 
-var_dump($username,$password,$password2);
+if ($username == "")
+{
+    exit("用户名不能为空");
+}
+
+if($password != $password2)
+{
+    exit("密码不同");
+}
+
+$dbhost ="localhost";
+$dbuser = "ltziben";
+$dbpwd = "ltzb2016";
+$dbname = "ltziben";
+
+$connetion = mysql_connect($dbhost, $dbuser, $dbpwd);
+mysql_set_charset('utf8',$connetion);
+
+if($connetion){
+    
+    $password = md5($password);
+    mysql_select_db($dbname, $connetion);
+    $dbsql = "INSERT INTO userinfo (UserName,Password,InvitationCode) VALUES('$username','$password','$username' )";
+    //exit($dbsql);
+    $dbresult = mysql_query($dbsql);
+    
+    if($dbresult){
+        echo $username."注册成功";
+    }else{
+        die($username."注册失败");
+    }
+}
+else {
+    echo $dbname."数据库连接失败";
+}
 
 ?>
