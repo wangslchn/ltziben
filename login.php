f141263da46b8bbe6326f71fbc23e9647ae23b60
wangslchn <wangsl@yeah.net>
2016-05-31 17:27:53 +0800

    代码格式化显示，能够完成正常登录页面

--- login.php
+++ login.php
@@ -1,49 +1,43 @@
 <?php
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-
-if ($username == "")
-{
-    //exit("用户名不能为空");
+if ($username == "") {
+    // exit("用户名不能为空");
     die('用户名不鞥你为空');
 }
 
-if($password == "")
-{
+if ($password == "") {
     exit("密码不能为空");
 }
 
-$dbhost ="localhost";
+$dbhost = "localhost";
 $dbuser = "ltziben";
 $dbpwd = "ltzb2016";
 $dbname = "ltziben";
 
 $connetion = mysql_connect($dbhost, $dbuser, $dbpwd);
-if($connetion){
+if ($connetion) {
     
     $password = md5($password);
     mysql_select_db($dbname, $connetion);
     $dbsql = "SELECT * FROM  userinfo WHERE UserName='$username' AND Password = '$password'";
-    //exit($dbsql);
+    // exit($dbsql);
     $dbresult = mysql_query($dbsql);
     
-    if($dbresult){
+    if ($dbresult) {
         $row = mysql_fetch_array($dbresult);
-        if($row){
-            echo $row['UserName']."登录成功";
-            //var_dump($row);
-        }else{
+        if ($row) {
+            echo $row['UserName'] . "登录成功";
+            // var_dump($row);
+        } else {
             die("用户名密码不正确，或者该用户不存在！");
         }
-        
-    }else{
+    } else {
         die("登录失败");
     }
-}
-else {
-    echo $dbname."连接数据库失败";
+} else {
+    echo $dbname . "连接数据库失败";
 }
 
 mysql_close($connetion);
--- register.php
+++ register.php
@@ -1,44 +1,39 @@
 <?php
-
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 $password2 = $_POST['password2'];
 
-if ($username == "")
-{
+if ($username == "") {
     exit("用户名不能为空");
 }
 
-if($password != $password2)
-{
+if ($password != $password2) {
     exit("两次密码输入不同");
 }
 
-$dbhost ="localhost";
+$dbhost = "localhost";
 $dbuser = "ltziben";
 $dbpwd = "ltzb2016";
 $dbname = "ltziben";
 
 $connetion = mysql_connect($dbhost, $dbuser, $dbpwd);
-mysql_set_charset('utf8',$connetion);
+mysql_set_charset('utf8', $connetion);
 
-if($connetion){
+if ($connetion) {
     
     $password = md5($password);
     mysql_select_db($dbname, $connetion);
     $dbsql = "INSERT INTO userinfo (UserName,Password,InvitationCode) VALUES('$username','$password','$username' )";
-    //exit($dbsql);
+    // exit($dbsql);
     $dbresult = mysql_query($dbsql);
     
-    if($dbresult){
-        echo $username."注册成功";
-    }else{
-        die($username."注册失败");
+    if ($dbresult) {
+        echo $username . "注册成功";
+    } else {
+        die($username . "注册失败");
     }
-}
-else {
-    echo $dbname."数据库连接失败";
+} else {
+    echo $dbname . "数据库连接失败";
 }
 
 mysql_close($connetion);
